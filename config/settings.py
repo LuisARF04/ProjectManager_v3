4dd9265f91e5917f8c6c97997d036612aa199332
Amanda <amanda7u7maya@gmail.com>
2025-12-27 20:20:12 -0500

    Cositas 1.2

--- config/settings.py
+++ config/settings.py
@@ -151,4 +151,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = f'Project Manager <{os.getenv('EMAIL_HOST_USER')}>'
+DEFAULT_FROM_EMAIL = f"Project Manager <{os.getenv('EMAIL_HOST_USER')}>"
